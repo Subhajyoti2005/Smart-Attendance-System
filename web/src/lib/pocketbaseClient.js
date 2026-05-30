b8378d8417fbb5f6ecb4d8d6159e7a1b79ef8c5c
Subhajyoti2005 <chakrabortysubhajyoti177@gmail.com>
2026-05-30 17:09:05 +0530

    Initial commit

---
+++ web/src/lib/pocketbaseClient.js
@@ -0,0 +1,5 @@
+import PocketBase from 'pocketbase';
+
+// const pb = new PocketBase('http://192.168.0.159:8090'); 
+ const pb = new PocketBase('https://bennett-fibre-babies-collector.trycloudflare.com');
+export default pb;
